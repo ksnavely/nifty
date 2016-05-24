c2deabb8229e969048dd5f3df3c71906e591bdc7
Kyle Snavely <ksnavely@cloudant.com>
2016-05-23 21:46:52 -0400

    missing semicolon

--- hello/hello.c
+++ hello/hello.c
@@ -9,6 +9,6 @@ static ERL_NIF_TERM hello(ErlNifEnv * env, int argc, const ERL_NIF_TERM argv[])
 static ErlNifFunc nif_funcs[] =
 {
     {"hello", 0, hello}
-}
+};
 
 ERL_NIF_INIT(hello, nif_funcs, NULL, NULL, NULL, NULL)
